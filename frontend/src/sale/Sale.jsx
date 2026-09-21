ba87872a38947da3d73bebf931ce2feff59b5e75
Breno-Barbosa1 <brenobarbosa117@gmail.com>
2026-09-21 16:45:11 -0300

    fix: Improved searched products table and current sale items table

--- frontend/src/sale/Sale.jsx
+++ frontend/src/sale/Sale.jsx
@@ -20,6 +20,11 @@ function Sale() {
   }
 
   const searchProducts = async () => {
+      if (!searchQuery.trim()) {
+          alert("Digite o nome do produto para pesquisar.");
+          return;
+      }
+      
       try {
           const response = await fetch(`http://127.0.0.1:8000/api/v1/products/search/${encodeURIComponent(searchQuery)}?page=${page}&size=${pageSize}`, {
             method: "GET",
@@ -50,12 +55,16 @@ function Sale() {
               <li><a href="/">Painel de Vendas</a></li>
           </ul>
           <div className="nav-actions-sales">
-              <button className="logout-btn-sales" onClick={handleLogout}> Sair </button>
+              <button className="logout-btn" onClick={handleLogout}> Sair </button>
           </div>
       </nav>
-      <div className="sales-dashboard">
+      <div className="sales-panels">
+
+    <div className="sales-dashboard">
         <div className="sales-dashboard-content">
-            <h2>Painel de Vendas</h2>
+
+            <h2>Lista de Produtos</h2>
+
             <div className="search-container">
                 <input
                     type="text"
@@ -63,85 +72,132 @@ function Sale() {
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
                 />
-                <button onClick={searchProducts}>Pesquisar</button>
+
+                <button onClick={searchProducts}>
+                    Pesquisar
+                </button>
             </div>
-            <table className="sales-table">
-                    <thead>
+            <table className="add-sales-table">
+                <thead>
+                    <tr>
+                        <th>ID_Produto</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th>Estoque</th>
+                        <th>Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {searchedProducts.length > 0
+                        ? searchedProducts.map((product) => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.product_name}</td>
+                                <td>R$ {product.selling_price}</td>
+                                <td>{product.stock_quantity}</td>
+                                <td>
+                                    <button onClick={() => {
+                                        if (product.stock_quantity > 0) {
+                                            setSaleItems((saleItems) => {
+                                                const existingItem = saleItems.find(
+                                                    (item) => item.id === product.id
+                                                )
+
+                                                if (existingItem) {
+                                                    return saleItems.map((item) =>
+                                                        item.id == product.id
+                                                            ? {...item, quantity: item.quantity + 1}
+                                                            : item 
+                                                    )       
+                                                }
+                                            
+                                            const saleItem = {
+                                                id: product.id,
+                                                product_name: product.product_name,
+                                                price: product.selling_price,
+                                                quantity: 1
+                                            };
+
+                                            return [...saleItems, saleItem]
+                                            })
+                                        } else {
+                                            alert("Produto sem estoque!");
+                                        }
+                                    }}>
+                                        Adicionar à Venda
+                                    </button>
+                                </td>
+                            </tr>
+                        ))
+                        :
                         <tr>
-                            <th>ID</th>
-                            <th>Nome</th>
-                            <th>Preço</th>
+                            <td colSpan="5" style={{ textAlign: 'center' }}>
+                                Digite o nome do produto e clique em "Pesquisar" para ver os resultados.
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {searchedProducts.length > 0
-                            ? searchedProducts.map((product) => (
-                                <tr key={product.id}>
-                                    <td>{product.id}</td>
-                                    <td>{product.product_name}</td>
-                                    <td>R$ {product.selling_price}</td>
-                                </tr>
-                            ))
-                            :
-                                <tr>
-                                    <td colSpan="5" style={{ textAlign: 'center' }}>
-                                        Digite o nome do produto e clique em "Pesquisar" para ver os resultados.
-                                    </td>
-                                </tr>
-                            }
-                    </tbody>
-                </table>
-                <div className="pagination">
+                    }
+                </tbody>
+            </table>
+            <div className="pagination">
+                <button
+                    className="pagination-btn"
+                    onClick={() => setPage(page - 1)}
+                    disabled={page === 1}
+                >
+                    ← Anterior
+                </button>
+
+                <span className="pagination-info">
+                    Página {page} de {totalPages}
+                </span>
+                {totalPages > 1 ? (
                     <button
                         className="pagination-btn"
-                        onClick={() => setPage(page - 1)}
-                        disabled={page === 1}
+                        onClick={() => setPage(page + 1)}
+                        disabled={page === totalPages}
                     >
-                        ← Anterior
+                        Próxima →
                     </button>
-                    <span className="pagination-info">
-                        Página {page} de {totalPages}
-                    </span>
-                    {totalPages > 1 ? (
-                        <button
-                            className="pagination-btn"
-                            onClick={() => setPage(page + 1)}
-                            disabled={page === totalPages}
-                        >
-                            Próxima →
-                        </button>
-                    ) : (
-                        <button className="pagination-btn" disabled>
-                            Próxima →
-                        </button>
-                    )}
-                </div>
+                ) : (
+                    <button className="pagination-btn" disabled>
+                        Próxima →
+                    </button>
+                )}
             </div>
-            <div className="current-sale-items">
+        </div>
+    </div>
+    <div className="current-sale-items-dashboard">
+        <div className="current-sale-items-content">
             <h1>Itens da Venda</h1>
-            {saleItems.length > 0 ? (
                 <table className="sale-items-table">
                     <thead>
                         <tr>
-                            <th>ID</th>
+                            <th>ID_Produto</th>
                             <th>Nome</th>
                             <th>Preço</th>
+                            <th>Quantidade</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        {saleItems.map((item) => (
-                            <tr key={item.id}>
-                                <td>{item.id}</td>
-                                <td>{item.product_name}</td>
-                                <td>R$ {item.selling_price}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            ) : (
-                <p>Nenhum item adicionado à venda.</p>
-            )}
-        </div>
+                        <tbody>
+                            {saleItems.length > 0 ? 
+                            saleItems.map((item) => (
+                                <tr key={item.id}>
+                                    <td>{item.id}</td>
+                                    <td>{item.product_name}</td>
+                                    <td>R$ {item.price}</td>
+                                    <td>{item.quantity}</td>
+                                </tr>
+                            )) : (
+                                <tr>
+                                    <td colSpan="4" style={{ textAlign: 'center' }}>
+                                        Nenhum item adicionado à venda.
+                                    </td>
+                                </tr>
+                        )}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
   )
